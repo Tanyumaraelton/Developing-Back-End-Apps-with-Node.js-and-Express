fc0cbe99ded3ac241d2e77faf5c022d42f0adfbc
Elton tanyumara <24b11cs525@adityauniversity.in>
2026-05-28 14:37:48 +0530

    Add files via upload

---
+++ Final_project/router/auth_users.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [
+  {
+    username: "Tonny",
+    password: "1234"
+  }
+];
+
+const isValid = (username) => {
+  let usersWithSameName = users.filter((user) => {
+    return user.username === username;
+  });
+  if (usersWithSameName.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const authenticatedUser = (username, password) => {
+  let validUsers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  if (validUsers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//only registered users can login
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 * 60 }
+    );
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send("Review deleted successfully");
+  } else {
+    return res
+      .status(208)
+      .json({ message: "Invalid Login. Check username and password" });
+  }
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  const review = req.body.review;
+  const isbn = req.params.isbn;
+
+  if (!review) {
+    return res.status(403).json({ message: "Please write your review!" });
+  }
+
+  const bookToReview = books[isbn];
+
+  if (!bookToReview) {
+    return res.status(403).json({ message: "Book not found!" });
+  } else {
+    const newReviewId = Object.keys(bookToReview.reviews).length + 1;
+    bookToReview.reviews[newReviewId] = review;
+    return res.status(200).json({ message: "Review book successfully" });
+  }
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
---
+++ Final_project/router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+      1: {"author": "Tonny","title": "trees", "reviews": "" },
+      2: {"author": "LAmela","title": "Stones", "reviews": {} },
+      3: {"author": "Denzel","title": "Roads", "reviews": {} },
+      4: {"author": "Unknown","title": "houses", "reviews": {} },
+      5: {"author": "Unknown","title": "Roofs", "reviews": {} },
+      6: {"author": "Unknown","title": "Money", "reviews": {} },
+      7: {"author": "Unknown","title": "Grass", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Life", "reviews": {} },
+      9: {"author": "jastice","title": "groot", "reviews": {} },
+      10: {"author": "benny","title": "Tables", "reviews": {} }
+}
+
+module.exports=books;
